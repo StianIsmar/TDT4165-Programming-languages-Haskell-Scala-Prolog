e38cc89b8508838c92b2f4ee1f48ed6e06e34ea8
StianIsmar <41960375+StianIsmar@users.noreply.github.com>
2018-12-09 15:11:11 +0100

    Completed a-d)

--- Scala/Main.scala
+++ Scala/Main.scala
@@ -1,3 +1,29 @@
 object Hello extends App {
-  println ("Hello Worldddd")
+
+
+  val x: Array[Int] = Array(1, 2, 3)
+  println(s"There are ${x.length} elements")
+  for (i <- x) println(i)
+
+
+  // 1b)
+
+  var generated: Array[Int] = Array()
+  for (i <- 1 to 50) generated :+= i
+
+  // 1c)
+  // Append to the array values 51 to 100 using MAP and anon func
+  (51 to 100).map((x: Int) => generated :+= x)
+  println("generated" + generated.mkString(" "))
+
+
+  //d)
+  // Create a function that sums the elements in an array of integers
+  // using a for-loop
+  def sumElements(input: Array[Int]): Int ={
+    var counter: Int = 0
+    for (i <- input) counter += i
+    counter
+  }
+
 }
