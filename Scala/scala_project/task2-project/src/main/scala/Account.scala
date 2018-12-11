5c341f64a17e6db0bf85cca00e4f09f52ac0d4ad
StianIsmar <41960375+StianIsmar@users.noreply.github.com>
2018-12-11 19:29:03 +0100

    Completed 3 functions in part 2

--- Scala/scala_project/task2-project/src/main/scala/Account.scala
+++ Scala/scala_project/task2-project/src/main/scala/Account.scala
@@ -24,12 +24,16 @@ class Account(val accountId: String, val bankId: String, val initialBalance: Dou
 
     def getTransactions: List[Transaction] = {
         // Should return a list of all Transaction-objects stored in transactions
-        ???
+        transactions.values.toList
     }
 
     def allTransactionsCompleted: Boolean = {
         // Should return whether all Transaction-objects in transactions are completed
-        ???
+        val listofTrans = getTransactions
+        for (i<-listofTrans)
+            if !(i.isCompleted) false
+            else true
+
     }
 
     def withdraw(amount: Double): Unit = ??? // Like in part 1
