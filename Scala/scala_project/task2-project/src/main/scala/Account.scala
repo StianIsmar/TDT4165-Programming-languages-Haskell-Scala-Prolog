ffd45a60c387b9e6e25a434510a96b46e1e63d68
StianIsmar <41960375+StianIsmar@users.noreply.github.com>
2018-12-12 11:25:42 +0100

    Work done on part 2 of the scala project

--- Scala/scala_project/task2-project/src/main/scala/Account.scala
+++ Scala/scala_project/task2-project/src/main/scala/Account.scala
@@ -29,20 +29,37 @@ class Account(val accountId: String, val bankId: String, val initialBalance: Dou
 
     def allTransactionsCompleted: Boolean = {
         // Should return whether all Transaction-objects in transactions are completed
-        val listofTrans = getTransactions
+        val listofTrans: List[Transaction] = getTransactions
+        val b: Boolean = true
         for (i<-listofTrans)
-            if !(i.isCompleted) false
-            else true
+            if !(i.isCompleted) b = false
+        b
 
     }
 
-    def withdraw(amount: Double): Unit = ??? // Like in part 1
-    def deposit(amount: Double): Unit = ??? // Like in part 1
-    def getBalanceAmount: Double = ??? // Like in part 1
+    def withdraw(amount: Double): Unit ={
+        balance.synchronized{
+            if (balance.amount - amount < 0) throw new NoSufficientFundsException
+            if (amount <= 0) throw ned IllegalAmountException
+              balance.amount -= amount
+        }
+    }
+    def deposit(amount: Double): Unit = {
+        balance.synchronized{
+            if (amount <=0) throw new IllegalAmountException
+            balance.amount += amount
+        }
+    }
+    def getBalanceAmount: Double = {
+        balance.amount
+
+    }
 
     def sendTransactionToBank(t: Transaction): Unit = {
         // Should send a message containing t to the bank of this account
-        ???
+        var bankRef: ActorRef = BankManager.findBank(bankId)
+        bankRef ! t
+
     }
 
     def transferTo(accountNumber: String, amount: Double): Transaction = {
@@ -65,29 +82,36 @@ class Account(val accountId: String, val bankId: String, val initialBalance: Dou
     }
 
     def reserveTransaction(t: Transaction): Boolean = {
-      if (!transactions.contains(t.id)) {
-        transactions += (t.id -> t)
-        return true
-      }
-      false
+        if (!transactions.contains(t.id)) {
+            transactions += (t.id -> t)
+            return true
+        }
+        false
     }
 
     override def receive = {
-		case IdentifyActor => sender ! this
+        case IdentifyActor => sender ! this
 
-		case TransactionRequestReceipt(to, transactionId, transaction) => {
-			// Process receipt
-			???
-		}
+        case TransactionRequestReceipt(to, transactionId, transaction) => {
+            // Process receipt
+            if (transactions.contains(transactionId)){ // den er i transactions
+                var transac = transactions.get(transactionId) // get it from the hashmap
+                var trans = transac.get //To get the actual Transaction, and
+                // not Option[Transaction]
+                trans.status = transaction.status
+                if (!transaction.isSuccessful){
+                    this.deposit(transaction.amount)
+                }
+            }
+        }
+        //Return current balance OK
+        case BalanceRequest => sender ! balance.amount
+        case t: Transaction => {
+            // Handle incoming transaction
+            ???
+        }
 
-		case BalanceRequest => ??? // Should return current balance
-
-		case t: Transaction => {
-			// Handle incoming transaction
-			???
-		}
-
-		case msg => ???
+        case msg => ???
     }
 
 
